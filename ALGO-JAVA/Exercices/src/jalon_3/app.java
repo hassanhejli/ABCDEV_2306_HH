a06bda38b8b8eed1761a00b3b471dac44d450f51
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-07-25 10:09:57 +0200

    correction jalon 3

--- ALGO-JAVA/Exercices/src/jalon_3/app.java
+++ ALGO-JAVA/Exercices/src/jalon_3/app.java
@@ -16,33 +16,33 @@ public class app {
 	 DEBUT DU PROGRAMME
 	 DEBUT FAIRE TANT QUE
 	 FAIRE
-	 	Ecrire "saisir un nombre de 1 à 10"
-	 	Lire nombre
-	 DEBUT SI
+	 Ecrire "saisir un nombre de 1 à 10"
+	 Lire nombre
+	 	DEBUT SI
 	 	si nombre >=1 et <= 10
 	 	saisieValide <-- vrai
-	 DEBUT POUR
-	 	POUR i allant de 0 à 10
-	 	resultat <-- i * nombre
-	 	Ecrire i, "*", nombre, "=", resultat
-	 	Ecrire i * nombre = nombre
-	 FIN POUR
-	 DEBUT FAIRE TANT QUE
-	 FAIRE
-	 	Ecrire "souhaitez vous afficher une autre table oui(O) ou non(N)"
-	 	Lire saisieUtilisateur
-	 DEBUT SI
-	 	SI saisieUtilisateur = N
-	 	Alors fermetureProg = vrai
-	 	Alors saisieValide = vrai
-	 	sinon si saisieUtilisateur = O
-	 	Alors FermetureProg = false
-	 	Alors saisieValide = vrai
-	 	sinon saisieValide = faux
-	 FIN SI
-	 tant que saisieValide est faux
-	 FIN SI
-	 tant que fermetureProg est faux
+	 		DEBUT POUR
+	 		POUR i allant de 0 à 10
+	 		resultat <-- i * nombre
+	 		Ecrire i, "*", nombre, "=", resultat
+	 		Ecrire i * nombre = nombre
+	 		FIN POUR
+	 			DEBUT FAIRE TANT QUE
+	 			FAIRE
+	 			Ecrire "souhaitez vous afficher une autre table oui(O) ou non(N)"
+	 			Lire saisieUtilisateur
+	 				DEBUT SI
+	 				SI saisieUtilisateur = N
+	 				Alors fermetureProg = vrai
+	 				Alors saisieValide = vrai
+	 				sinon si saisieUtilisateur = O
+	 				Alors FermetureProg = false
+	 				Alors saisieValide = vrai
+	 				sinon saisieValide = faux
+	 				FIN SI
+	 			tant que saisieValide est faux
+	 	FIN SI
+	 	tant que fermetureProg est faux
 	FIN DU PROGRAMME
 	 */
 	public static void main(String[] args) {
@@ -52,28 +52,53 @@ public class app {
 		int i;
 		int resultat;
 		boolean fermetureProg;
+		boolean saisieValide;
 		String saisieUtilisateur;
 		
 		
 		// on ouvre le scanner
 		Scanner hasscan = new Scanner(System.in);
-	
-	
-		// on demande a l'utilisateur d'ecrire un nombre entre 1 et 10
-		System.out.println("Saisir un nombre entre 1 et 10");
 		
-		// on affecte la variable nombre
-		nombre = hasscan.nextInt();
 		
-		// on fait une boucle for pour afficher la table du nombre de 0 à 10
-		for(i = 0; i <= 10; i++)
-		{
-			resultat = i * nombre;
-			System.out.println(i +  " * " + nombre +  " = " + resultat);
-		}
-		
-		// on ferme le scanner
-		hasscan.close();
-	}
+		do
+			{
+			
+			System.out.println("Saisir un nombre entre 1 et 10");
+			nombre = hasscan.nextInt();
+				if (nombre >= 1 && nombre <= 10)
+				{
+					saisieValide = true;
+					for (i=0; i<=10; i++)
+					{
+						resultat = i * nombre;
+						System.out.println(i + " * " + nombre + " = " + resultat);
+					}
+						do
+						{
+							System.out.println("souhaitez vous afficher une autre table oui(O) ou non(N)");
+							saisieUtilisateur = hasscan.next();
+							if (saisieUtilisateur.equals("n") || saisieUtilisateur.equals("N"))
+							{
+								fermetureProg = true;
+								saisieValide = true;
+							}
+							else if (saisieUtilisateur.equals("o") || saisieUtilisateur.equals("O"))
+							{
+								fermetureProg = false;
+								saisieValide = true;
+							}
+							else
+							{
+								saisieValide = false;
+							}
+						}while(!saisieValide);
+				}
+				
+			}
+		while(fermetureProg = false);
+			
+			hasscan.close();
 
-}
+		}
+
+	}
