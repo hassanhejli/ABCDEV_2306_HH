8952dfa8d05e05d3e433da82403fb7dcc8faf0fc
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-07-27 12:23:27 +0200

    fin de jalon_algo_niveau4

--- ALGO-JAVA/Exercices/src/jalon_algo_niveau4/app.java
+++ ALGO-JAVA/Exercices/src/jalon_algo_niveau4/app.java
@@ -6,29 +6,36 @@ public class app {
 
 	public static void main(String[] args) {
 		
+		// on initialise les variables
 		int position = 2;
 		int nb;
-		String resultat = "t0\t1";
+		String resultat = "\t0\n\t1";
 		long nbPrecedent = 0;
 		long nbCourant = 1;
 		long nbSuivant;
 		
+		// on ouvre le scanner
 		Scanner hasscan = new Scanner(System.in);
 		
-		System.out.println("Combien de nombres de la suite de Fibonacci souhaitez vous affiché: ");
+		
+		do // tant que l'utilisateur ne tape pas 0, faire
+		{
+			
+		// on affiche les message a l'utilisateur
+		System.out.println("Combien de nombres de la suite de Fibonacci souhaitez vous affiches: ");
 		System.out.println("tapez la touche 0 pour quitter le programme");
 		nb = hasscan.nextInt();
 		
-		
-		if(nb!=0) // si nombre est inegal a zero
-		{
-			
-			if (nb > 2) // si nombre est superieur a 2
+			if (nb == 0) // si nombre egal zero sortir du programme 
 			{
-				while(position < nb) // tant que position est inferieur a nombre
+				System.exit(0);
+			}
+			else if (nb > 2) // sinon si nombre est superieur a 2
+			{
+				while(position < nb) // tant que position est inferieur a nombre faire
 				{
 					nbSuivant = nbPrecedent + nbCourant;
-					resultat = resultat + "n\t" + nbSuivant;
+					resultat = resultat + "\n\t" + nbSuivant;
 					nbPrecedent = nbCourant;
 					nbCourant = nbSuivant;
 					position +=1;	
@@ -39,14 +46,16 @@ public class app {
 			nb = 2;
 			}
 		
+			// on affiche les resultats a l'utilisateur
 			System.out.println("les " + nb + " premiers nombres de la suite de Fibonacci sont \n" + resultat);
-			System.out.println(nb + " nombres affichés");
+			System.out.println(nb + " nombres affiches");
 		}
 	
-	else //sinon si nombre = 0 alors sortie du programme
-	{
-		System.exit(0);
-	}
+
+	while (nb!=0); 
+	
+		
+	// on ferme le scanner
 	hasscan.close();
 }
 }
