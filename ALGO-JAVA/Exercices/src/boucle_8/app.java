ac17aec31d2ea1fdafdd7c46b007b42a18ac75dd
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-07-27 12:36:52 +0200

    mise a jour boucle_8

--- ALGO-JAVA/Exercices/src/boucle_8/app.java
+++ ALGO-JAVA/Exercices/src/boucle_8/app.java
@@ -11,6 +11,7 @@ public class app {
 		int paye;
 		int rendu;
 		int i = 0;
+		int j;
 		
 		
 		Scanner hasscan = new Scanner(System.in);
@@ -34,15 +35,17 @@ public class app {
 			if (rendu >= 10)
 			{
 				rendu -= 10;
-				System.out.println("votre monnaie de 10");
+				System.out.println("pour votre monnaie voici un billet de 10 s'il vous plait ");
 			}
 			else if (rendu >= 5)
 			{
 				rendu -= 5;
+				System.out.println("pour votre monnaie voici un billet de 5 s'il vous plait ");
 			}
 			else 
 			{
 				rendu -= 1;
+				System.out.println("pour votre monnaie voici une piece de 1 s'il vous plait");
 			}
 		}
 	}
