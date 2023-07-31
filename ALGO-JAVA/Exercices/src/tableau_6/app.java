4a2d58a00cae9531b059bc5bf6ae1bc76bb729f6
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-07-31 14:18:17 +0200

    exercice tableau_5 resolu

--- ALGO-JAVA/Exercices/src/tableau_6/app.java
+++ ALGO-JAVA/Exercices/src/tableau_6/app.java
@@ -6,29 +6,33 @@ public class app {
 
 	public static void main(String[] args) {
 		
-		// on ouvre le scanner
-		Scanner hasscan = new Scanner(System.in);
+		
 		
 		// on declare les variables
 		int tab1[] = {4,8,7,12};
 		int tab2[] = {3,6};
 		int tab3[] = new int [4];
 		int i;
+		int j;
+		int somme = 0;
 		
+		// on ouvre le scanner
+				Scanner hasscan = new Scanner(System.in);
+				
 		// on boucle pour multiplier les tableaux 1 et 2 et on affiche le resultat en console
-		for (i=0; i<tab1.length; i++)
+		for (i = 0; i < tab1.length; i++)
 		{
-			tab3[i] = tab1[i] * tab2[i];
-			System.out.println(tab3[i]);
+			
+			// on boucle pour additionner les valeurs obtenue et on affiche le resultat en console
+			for (j = 0; j < tab2.length; j++)
+			{
+				somme += tab1[i] * tab2[j];
+				System.out.println((tab1[i] + " * " + tab2[j]) + " = " + (tab1[i]  *  tab2[j]));// pour verifier les calculs
+				System.out.println(somme);
+			}
 		}
-		
-		// on boucle pour additionner les valeurs obtenue et on affiche le resultat en console
-		for (i=0; i<tab1.length; i++)
-		{
-			tab3[i] = tab3[0] + tab3[1] + tab3[2] + tab3[3];
-			System.out.println(tab3[i]);
-		}
-		
+		System.out.println(somme);
+
 		// on ferme le scanner
 		hasscan.close();
 		
