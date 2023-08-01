bc40cd15edeb5037a45455d1073bb7b024352792
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-08-01 09:34:26 +0200

    commit exercice tableau_8

---
+++ ALGO-JAVA/Exercices/src/tableau_8/app.java
@@ -0,0 +1,49 @@
+package tableau_8;
+
+import java.util.Scanner;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+		// on declare les variables
+				int vali, i, plusGrand = 0;
+						
+		// on ouvre le scanner
+		Scanner hasscan = new Scanner(System.in);
+				
+		// on demande a l'utilisateur de saisir le nombre de valeur qu'il va entrer
+		System.out.println(" saisissez le nombre de valeurs que vous souhaitez saisir dans le tableau");
+				
+		// on lit le nombre de valeur
+		vali = hasscan.nextInt();
+				
+		// on declare un tableau qui comporte le nombre de valeur entré
+				int tabNb[]  = new int [vali];
+				
+		// on demande a l'utilisateur de saisir les valeurs
+		System.out.println("saisissez vos valeurs");
+		
+		// on bouvle pour entrer les valeurs dans le tableau
+		for (i = 0; i < vali; i++)
+		{
+			tabNb[i] = hasscan.nextInt();
+		}
+		
+		// on boucle pour afficher les valeurs dans la console
+		for (i = 0; i < tabNb.length; i++)
+		{
+			
+			if(tabNb[i] >= plusGrand)
+			{
+				plusGrand = tabNb[i];
+			}
+		}
+
+		System.out.println("Le nombre le plus grand est : "+ plusGrand);
+		
+		// on ferme le scanner
+		hasscan.close();
+	}
+
+}
