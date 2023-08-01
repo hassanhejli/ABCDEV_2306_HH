08c9a61b9cbabcea87f9ebc26376c3bcabb4c7c7
hassanhejli <137295077+hassanhejli@users.noreply.github.com>
2023-08-01 08:25:42 +0200

    commit exercice tableau_7

--- ALGO-JAVA/Exercices/src/tableau_7/app.java
+++ ALGO-JAVA/Exercices/src/tableau_7/app.java
@@ -5,26 +5,27 @@ import java.util.Scanner;
 public class app {
 
 	public static void main(String[] args) {
-		int nomb;
-		int vali;
-		int i;
-		int sommePos = 0;
-		int sommeNeg = 0;
+		
+		// on declare les variables
+		int vali, i;
+		
+		
 		
 		// on ouvre le scanner
 		Scanner hasscan = new Scanner(System.in);
 		
 		// on demande a l'utilisateur de saisir le nombre de valeur qu'il va entrer
 		System.out.println(" saisissez le nombre de valeurs que vous souhaitez saisir dans le tableau");
+		
 		// on lit le nombre de valeur
 		vali = hasscan.nextInt();
+		
 		// on declare un tableau qui comporte le nombre de valeur entré
 		int tabNb[]  = new int [vali];
 		
 		// on demande a l'utilisateur de saisir les valeurs
 		System.out.println("saisissez vos valeurs");
-		// on lit la valeur
-		nomb = hasscan.nextInt();
+		
 		
 		// pour i en entree a zero et en sortie egale au nombre de valeur entré par l'utilisateur on repete l'action
 		for (i = 0; i < vali; i++)
@@ -33,7 +34,11 @@ public class app {
 			tabNb[i]+=1;
 		}
 		
-		System.out.print(tabNb [i]);
+		// on refait une deuxieme boucle pour la lecture du tableau
+		for (i = 0; i < tabNb.length; i++)
+		{
+			System.out.println(tabNb [i]);
+		}
 		
 		// on ferme le scanner
 		hasscan.close();
